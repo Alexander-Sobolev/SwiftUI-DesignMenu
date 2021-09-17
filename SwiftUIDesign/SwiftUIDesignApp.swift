cfdb0adf361f5a5a046910d04885242e98db477a
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-09-17 14:17:01 +0300

    Initial Commit

---
+++ SwiftUIDesign/SwiftUIDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignApp.swift
+//  SwiftUIDesign
+//
+//  Created by Alexander Sobolev on 17.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

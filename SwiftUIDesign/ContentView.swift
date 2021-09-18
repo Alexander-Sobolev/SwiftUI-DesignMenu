fc5a86bed8adb1b90ec6a2da372a0106d026c180
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-09-18 23:36:34 +0300

    Create SignUp

--- SwiftUIDesign/ContentView.swift
+++ SwiftUIDesign/ContentView.swift
@@ -185,11 +185,86 @@ struct Login: View {
                     .padding(.horizontal, 50)
                     .background(Color("Color"))
                     .clipShape(Capsule())
-                    .shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: 5)
+                    .shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: -5)
             }
             
             .offset(y: 35)
             .opacity(self.index == 0 ? 1 : 0)
+        }
+    }
+}
+
+struct SignUp: View {
+    @State var email = ""
+    @State var pass = ""
+    @State var repass = ""
+    @Binding var index: Int
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            VStack {
+                HStack {
+                    Spacer(minLength: 0)
+                    
+                    VStack(spacing: 10) {
+                        Text("SignUp")
+                            .foregroundColor(self.index == 1 ? .white : .gray)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Capsule()
+                            .fill(self.index == 1 ? Color.blue : Color.clear)
+                            .frame(width: 100, height: 5)
+                    }
+                }
+                .padding(.top, 30)
+                
+                VStack{
+                    HStack(spacing: 15) {
+                        Image(systemName: "eye.slash.fill")
+                            .foregroundColor(Color("Color2"))
+                        SecureField("Password", text: $pass)
+                    }
+                    Divider()
+                        .background(Color.white.opacity(0.5))
+                }.padding(.horizontal)
+                .padding(.top, 30)
+                
+                VStack {
+                    HStack(spacing: 15) {
+                        Image(systemName: "eye.slash.fill")
+                            .foregroundColor(Color("Color2"))
+                        SecureField("Password", text: $repass)
+                    }
+                    Divider().background(Color.white.opacity(0.5))
+                }.padding(.horizontal)
+                .padding(.top, 30)
+            }
+            .padding()
+            .padding(.bottom, 65)
+            .background(Color("Color2"))
+            .clipShape(CShapeNew())
+            .contentShape(CShapeNew())
+            .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: -5)
+            .onTapGesture {
+                self.index = 1
+            }
+            .cornerRadius(35)
+            
+            Button( action: {
+                //
+            }) {
+                Text("SIGNUP")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .padding(.horizontal, 50)
+                    .clipShape(Capsule())
+                    .shadow(color: .white.opacity(0.1), radius: 5, x: 0, y: -5)
+            }
+            .offset(y: 25)
+            .opacity(self.index == 1 ? 1 : 0)
+            
         }
     }
 }
